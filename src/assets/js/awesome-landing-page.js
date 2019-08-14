48f9aad98d47cdfdc5211a67ebe06709d49f3d19
Victor Ukafor <victorukafor@gmail.com>
2019-08-14 14:43:43 +0100

    Implement the landing page

---
+++ src/assets/js/awesome-landing-page.js
@@ -0,0 +1,112 @@
+/*!
+
+ =========================================================
+ * Awesome Landing Page - v1.2.2
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/awesome-landing-page
+ * Copyright 2017 Creative Tim (http://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/creativetimofficial/awesome-landing-page/blob/master/LICENSE.md)
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
+
+let big_image;
+$().ready(() => {
+  $('.selector').click(function () {
+    SelectColor(this);
+  });
+  const selectCol = 0;
+  if (selectCol == 0) {
+    if ($('body').hasClass('landing-page1')) {
+
+    }
+  }
+});
+
+$(window).on('scroll', () => {
+  responsive = $(window).width();
+  if (responsive >= 768) {
+    parallax();
+  }
+});
+
+function SelectColor(btn) {
+  oldColor = $('.filter-gradient').attr('data-color');
+  newColor = $(btn).attr('data-color');
+
+  oldButton = $('a[id^="Demo"]').attr('data-button');
+  newButton = $(btn).attr('data-button');
+
+  $('.filter-gradient').removeClass(oldColor).addClass(newColor).attr('data-color', newColor);
+
+  $('a[id^="Demo"]').removeClass(`btn-${oldButton}`).addClass(`btn-${newButton}`).attr('data-button', newButton);
+
+  $('.carousel-indicators').removeClass(`carousel-indicators-${oldColor}`).addClass(`carousel-indicators-${newColor}`);
+
+  $('.card').removeClass(`card-${oldColor}`).addClass(`card-${newColor}`);
+
+  $('.selector').removeClass('active');
+  $(btn).addClass('active');
+}
+
+$('.switch').each(function () {
+  const selector = $(this).parent('li');
+  $(this).click(function () {
+    if (selector.siblings().hasClass('active')) {
+      selector.addClass('active');
+      selector.siblings().removeClass('active');
+      const slide = $(this).attr('data-slide');
+      const lastClass = $('body').attr('class').split(' ').pop();
+      $('body').removeClass(lastClass);
+      $('body').addClass(`landing-page${slide}`);
+    }
+  });
+});
+
+var parallax = debounce(() => {
+  no_of_elements = 0;
+  $('.parallax').each(function () {
+    const $elem = $(this);
+
+    if (isElementInViewport($elem)) {
+      const parent_top = $elem.offset().top;
+      const window_bottom = $(window).scrollTop();
+      const $image = $elem.find('.parallax-background-image');
+      const $oVal = ((window_bottom - parent_top) / 3);
+      $image.css('margin-top', `${$oVal}px`);
+    }
+  });
+}, 6);
+
+function debounce(func, wait, immediate) {
+  let timeout;
+  return function () {
+    let context = this,
+      args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
+  };
+}
+
+
+function isElementInViewport(elem) {
+  const $elem = $(elem);
+
+  // Get the scroll position of the page.
+  const scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+  const viewportTop = $(scrollElem).scrollTop();
+  const viewportBottom = viewportTop + $(window).height();
+
+  // Get the position of the element on the page.
+  const elemTop = Math.round($elem.offset().top);
+  const elemBottom = elemTop + $elem.height();
+
+  return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
